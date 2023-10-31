8194da559e13b0147f80d55615c30d62f72d3a7c
Tushar <tushar.vaid027@gmail.com>
2023-11-01 02:06:41 +0530

    Update

--- static/js/contact.js
+++ static/js/contact.js
@@ -1,107 +1,126 @@
+window.addEventListener("beforeunload", function () {
+  document.documentElement.scrollTop = 0;
+});
+
+function homefunc() {
+  window.location.redirect('index.pug');
+}
 const nameid = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('text');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const textarea = document.querySelector('.mytextarea');
 
-nameid.addEventListener('blur',()=>{
-    let str = nameid.value; 
-    const popover = document.querySelector(`.popover[data-field="name"]`);
-
-    // Validate name here
-    let regex = /^(?!.*\s\s)[A-Za-z]+(?: [A-Za-z]+)?$/;
-    popover.textContent = nameid.getAttribute('data-error-message');
-    popover.style.display = 'block';
-    const inputBox = nameid.closest('.inputbox');
-    if(regex.test(str)){
-        popover.textContent = '';
-        popover.style.display = 'none';
-    }
-    else{
-        popover.style.left = '0';
-        popover.style.top = inputBox.offsetHeight + 'px';
-    }
+nameid.addEventListener('blur', () => {
+  let str = nameid.value;
+  const popover = document.querySelector(`.popover[data-field="name"]`);
+  // Validate name here
+  let regex = /^([a-zA-Z ]){1,31}$/;
+  popover.textContent = nameid.getAttribute('data-error-message');
+  popover.style.display = 'block';
+  const inputBox = nameid.closest('.inputbox');
+  if (regex.test(str)) {
+    popover.textContent = '';
+    popover.style.display = 'none';
+    input1.style.borderBottomColor = 'white';
+  }
+  else {
+    popover.style.left = '0';
+    popover.style.top = inputBox.offsetHeight + 'px';
+    input1.style.borderBottomColor = 'red';
+  }
 })
-email.addEventListener('blur',()=>{
-    let str = email.value; 
-    const popover = document.querySelector(`.popover[data-field="email"]`);
+email.addEventListener('blur', () => {
+  let str = email.value;
+  const popover = document.querySelector(`.popover[data-field="email"]`);
 
-    // Validate name here
-    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,5}$/;
-    popover.textContent = email.getAttribute('data-error-message');
-    popover.style.display = 'block';
-    const inputBox = email.closest('.inputbox');
-    if(regex.test(str)){
-        popover.textContent = '';
-        popover.style.display = 'none';
-    }
-    else{
-        popover.style.left = '0';
-        popover.style.top = inputBox.offsetHeight + 'px';
-    }
+  // Validate name here
+  let regex =/^[^@]+@[^.]+\.[a-z]{2,}(?:\.[a-z]{2})?$/;
+  popover.textContent = email.getAttribute('data-error-message');
+  popover.style.display = 'block';
+  const inputBox = email.closest('.inputbox');
+  if (regex.test(str)) {
+    popover.textContent = '';
+    popover.style.display = 'none';
+    input2.style.borderBottomColor = 'white';
+  }
+  else {
+    popover.style.left = '0';
+    popover.style.top = inputBox.offsetHeight + 'px';
+    input2.style.borderBottomColor = 'red';
+  }
 })
-message.addEventListener('blur',()=>{
-    let str = message.value; 
-    const popover = document.querySelector(`.popover[data-field="text"]`);
+function mymessage(){
+  let str = message.value;
+  const popover = document.querySelector(`.popover[data-field="text"]`);
+  // Validate name here
+  let regex = /^\s*$/g;
+  popover.textContent = message.getAttribute('data-error-message');
+  popover.style.display = 'block';
+  if (!regex.test(str)) {
+    popover.textContent = '';
+    popover.style.display = 'none';
+    textarea.style.borderBottomColor = 'white';
+  }
+  else {
+    popover.style.left = '10';
+    popover.style.top = 'px';
+    textarea.style.borderBottomColor = 'red';
+  }
+}
 
-    // Validate name here
-    popover.textContent = message.getAttribute('data-error-message');
-    popover.style.display = 'block';
-    const inputBox = message.closest('.inputbox');
-    if(str != ''){
-        popover.textContent = '';
-        popover.style.display = 'none';
-    }
-    else{
-        popover.style.left = '10';
-        popover.style.top ='px';
-    }
-})
+message.addEventListener('blur',mymessage);
 
 const form = document.getElementById('contactform');
 
-form.addEventListener('submit',(event) => {
+form.addEventListener('submit', (event) => {
   const nameValue = nameid.value;
   const emailValue = email.value.trim();
-  const messageValue = message.value.trim();
+  const messageValue = message.value;
   const submitButton = document.querySelector('button[type="submit"]');
 
-  event.preventDefault();
+
   // Validate each field
-  if (nameValue === '' || !isValidName(nameValue)) {
+  if (!isValidName(nameValue)) {
     // Show error for name or prevent submission if name is invalid
     nameid.value = '';
     event.preventDefault();
   }
-  else if (emailValue === '' || !isValidEmail(emailValue)) {
+  else if (!isValidEmail(emailValue)) {
     // Show error for email or prevent submission if email is invalid
     email.value = '';
     event.preventDefault();
   }
-   else if (messageValue === '') {
+  else if (!isvalidmessage(messageValue)) {
     // Show error for message or prevent submission if message is empty
-    message.value = '';
     event.preventDefault();
   }
-  else {
+  else{
     submitButton.textContent = 'Please wait...';
     setTimeout(() => {
-        form.submit();
-      },1500);
+      form.submit();
+    }, 3500);
   }
 });
 
 function isValidName(name) {
   // Return true if the name is valid, false otherwise
-  return /^(?!.*\s\s)[A-Za-z]+(?: [A-Za-z]+)?$/.test(name);
+  return /^([a-zA-Z ]){1,31}$/.test(name);
 }
 
 function isValidEmail(email) {
   // Return true if the email is valid, false otherwise
-  return /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,5}$/.test(email);
+  return /^[^@]+@[^.]+\.[a-z]{2,}(?:\.[a-z]{2})?$/.test(email);
+}
+function isvalidmessage(msg) {
+  return /^\s*$/g.test(msg);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Clear form fields on page load
-    document.getElementById("name").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("text").value = "";
-  });
+  // Clear form fields on page load
+  document.getElementById("name").value = "";
+  document.getElementById("email").value = "";
+  document.getElementById("text").value = "";
+  document.querySelector('button[type="submit"]').textContent = 'Submit';
+});
--- static/js/script.js
+++ static/js/script.js
@@ -1,99 +1,37 @@
 
-
-// window.addEventListener('scroll', function () {
-//   var secondsection = document.querySelector(".secondsection");
-//   var windowpos = window.scrollY;
-//   var pos = secondsection.position();
-//   console.log(pos.top);
-//   if (windowpos > pos.top && pos.top+500 >=windowpos) {
-//     secondsection.classList.add('visible');
-//   } else {
-//     secondsection.classList.remove('visible');
-//   }
-
-// })
-
+window.addEventListener("beforeunload", function() {
+  document.documentElement.scrollTop = 0;
+});
 // Logo reload
 function fun() {
   window.location.reload();
 }
 
-document.getElementById("linkedinlink").addEventListener("click", function() {
-  // window.location.href = "https://www.linkedin.com/in/tusharvaid/"; 
-     window.open("https://www.linkedin.com/in/tusharvaid/","_blank");
+document.getElementById("linkedinlink").addEventListener("click", function () {
+  window.open("https://www.linkedin.com/in/tusharvaid/", "_blank");
 });
 
 // Animation in HomePage
-window.addEventListener("load", function () {
-  var reveals = document.querySelectorAll('.reveal, .reveal2');
-  var reveals3 = document.querySelectorAll('.reveal3');
-  console.log("HI");
-  function handleReveal() {
-    var windowHeight = window.innerHeight;
 
-    for (var i = 0; i < reveals.length; i++) {
-      var elementTop = reveals[i].getBoundingClientRect().top;
+var reveal1 = document.getElementById("reveal1");
+var reveal2 = document.getElementById("reveal2");
 
-      if (elementTop < windowHeight) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
-    }
+var windowHeight = document.documentElement.clientHeight;
 
-    for (var i = 0; i < reveals3.length; i++) {
-      var elementTop = reveals3[i].getBoundingClientRect().top;
-      var revealpoint = 150;
-      if (elementTop < windowHeight - revealpoint) {
-        reveals3[i].classList.add("active2");
-      } else {
-        reveals3[i].classList.remove("active2");
-      }
-    }
+var reval1Y = reveal1.getBoundingClientRect().y;
+var reval2Y = reveal2.getBoundingClientRect().y;
+
+var carousel = document.getElementById('carouselExample');
+window.addEventListener("scroll", function () {
+  
+  // carousel animation
+  var carouselY = carousel.getBoundingClientRect().y;
+  // var carouselYbottom = carousel.getBoundingClientRect().bottom;
+  var carouselHeight = carousel.getBoundingClientRect().height;
+  if(windowHeight >carouselY + carouselHeight*1/3){
+    carousel.style.animation = 'transformcarousel 2s ease forwards';
   }
-
-  // Call handleReveal initially
-  handleReveal();
-
-  // Add a scroll event listener
-  window.addEventListener("scroll", handleReveal);
+  // else if(windowHeight> carouselHeight){
+  //   carousel.style.animation = 'transformcarouselback 2s ease backwards';
+  // }
 });
-
-// window.addEventListener("scroll", revealcontent);
-// function reveal() {
-//   console.log("HIII");
-//   var reveals = document.querySelectorAll('.reveal,.reveal2');
-//   // var reveals3 = document.querySelectorAll('.reveal3');
-//   for (var i = 0; i < reveals.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveals[i].getBoundingClientRect().top;
-//     // var elementVisible = 0;
-//     // var revealpoint = 150;
-
-//     if (elementTop < windowHeight) {
-//       reveals[i].classList.add("active");
-//       // reveals3[i].classList.add("active2");
-//     }
-//     else {
-//       reveals[i].classList.remove("active");
-//       // reveals3[i].classList.remove("active2");
-
-//     }
-//   }
-// }
-// function revealcontent() {
-//   var reveal = document.querySelector('.reveal3');
-//   for (var i = 0; i < reveal.length; i++) {
-//     var windowHeight = window.innerHeight;
-//     var elementTop = reveal[i].getBoundingClientRect().top;
-//     // var elementVisible = 0;
-//     var revealpoint = 150;
-//     if (elementTop < windowHeight - revealpoint) {
-//       reveal[i].classList.add("active2");
-//     }
-//     else {
-//       reveal[i].classList.remove("active2");
-//     }
-//   }
-// }
-
