c05baf9d601df153457a8a74c94adc65d423d209
Tushar <tushar.vaid027@gmail.com>
2023-12-25 16:42:05 +0530

    DB Update

--- db.js
+++ db.js
@@ -1,9 +1,5 @@
 const mongoose = require('mongoose');
 async function connectToMongo() {
-    // await mongoose.connect(`${process.env.MONGODB_URI}/Submit`,{
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    //   }).then(() => console.log("Connected to Mongodb Successfully")).catch(err => console.log(err));
       try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/submit`);
         // console.log(`${connectionInstance}`);
