a9545a5ef4ffc883dfd37edb22dc07722d8ee8f7
Tushar <tushar.vaid027@gmail.com>
2023-12-31 14:06:56 +0530

    vercel config

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require('express')
 const path = require("path");
 const bodyParser = require('body-parser');
 const User = require("./models/Users");
-const { prototype } = require('module');
+
 require('dotenv').config()
 
 connectToMongo();
 const app = express();
-const PORT = process.env.PORT || 5000
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
@@ -17,7 +16,7 @@ app.use('/static', express.static('static')); // For serving static files
 app.use(express.urlencoded());
 // Pug configuration
 app.set('view engine', 'pug'); //setting template engine as pug
-app.set('views', path.join(__dirname, 'views')) //setting the views directory
+app.set('views', path.join(__dirname, 'views')) //setting the public directory
 
 app.get('/', (req, res) => {
     res.status(200).render('index.pug');
@@ -26,7 +25,7 @@ app.get('/contact', (req, res) => {
     res.status(200).render('contact.pug');
 })
 
-app.post('/submit',(req, res) => {
+app.post('/submit', (req, res) => {
     const userdata = new User(req.body);
     userdata.save().then(() => {
         res.status(200).render('submit.pug');
@@ -34,6 +33,7 @@ app.post('/submit',(req, res) => {
         res.status(400).send("Server Error Pls wait..");
     })
 })
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
     console.log(`The application started successfully on port ${PORT}`);
 })
