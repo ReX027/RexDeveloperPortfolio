ab5fa31dc0429a13facdef6eea0831346faf1f24
Tushar <tushar.vaid027@gmail.com>
2024-01-15 18:28:16 +0530

    submit api updated

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,22 +1,21 @@
+function redirectToIndex() {
+  // Redirect to the main page route
+  window.location.href = "/";
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    const countdownElement = document.getElementById("countdown");
-    let countdown = 3; // Initial countdown value in seconds
-  
-    // Function to update the countdown element
-    function updateCountdown() {
-      countdownElement.textContent = countdown;
-      countdown--;
-  
-      if (countdown < 0) {
-        // Redirect to the main page route after 3 seconds
-        redirectToIndex();
-      }
+  const countdownElement = document.getElementById("countdown");
+  let countdown = 3; // Initial countdown value in seconds
+
+  // Function to update the countdown element
+  function updateCountdown() {
+    countdownElement.textContent = countdown;
+    countdown--;
+
+    if (countdown < 0) {
+      // Redirect to the main page route after 3 seconds
+      redirectToIndex();
     }
-    setInterval(updateCountdown,800);
-  });
-  
-  function redirectToIndex() {
-    // Redirect to the main page route
-    window.location.href = "/";
   }
-  
+  setInterval(updateCountdown, 800);
+});
